698f2c79ffff61ecae4e3004b4107c5e4ef02123
paulinhavgueiros <paulinhavgueiros@gmail.com>
2017-01-22 14:11:41 -0200

    storyboard correction

---
+++ PagarMachine/Utils.h
@@ -0,0 +1,22 @@
+//
+//  Utils.h
+//  PagarMachine
+//
+//  Created by Paula Vasconcelos Gueiros on 22/1/17.
+//  Copyright © 2016 paulinhavgueiros. All rights reserved.
+//
+
+#define kOperationPassword          1234
+
+#import <UIKit/UIKit.h>
+
+@interface Utils : NSObject
+
+@end
+
+
+
+
+
+
+
